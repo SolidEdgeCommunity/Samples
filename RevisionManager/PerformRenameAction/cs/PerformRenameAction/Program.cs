fb2af9e1b53ced031a4338c6cf585b18a94f5857
Jason Newell <jason.newell@outlook.com>
2015-08-10 02:15:05 -0500

    Added PerformRenameAction sample.

---
+++ RevisionManager/PerformRenameAction/cs/PerformRenameAction/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace PerformRenameAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For testing purposes, change the path to the .asm.
+            var assemblyPath = @"C:\Users\jason\Desktop\Asm3.asm";
+
+            // Start Revision Manager.
+            var application = new RevisionManager.Application();
+
+            // Open the assembly.
+            var assemblyDocument = (RevisionManager.Document)application.OpenFileInRevisionManager(assemblyPath);
+
+            // Get the linked documents.
+            var linkedDocuments = (RevisionManager.LinkedDocuments)assemblyDocument.LinkedDocuments;
+
+            // Allocate input arrays.
+            var ListOfInputFileNames = new List<string>();
+            var ListOfNewFileNames = new List<string>();
+            var ListOfInputActions = new List<RevisionManager.RevisionManagerAction>();
+
+            // Process each linked document.
+            for (int i = 1; i <= linkedDocuments.Count; i++)
+            {
+                // Get the specified linked document by index.
+                var linkedDocument = (RevisionManager.Document)linkedDocuments.Item[i];
+
+                // Get the current path, folder path, filename and extension to the linked document.
+                var linkedDocumentPath = linkedDocument.AbsolutePath;
+                var linkedDocumentDirectory = System.IO.Path.GetDirectoryName(linkedDocumentPath);
+                var linkedDocumentFilename = System.IO.Path.GetFileName(linkedDocumentPath);
+                var linkedDocumentExtension = System.IO.Path.GetExtension(linkedDocumentPath);
+
+                // Generate a new random filename for the linked document.
+                var linkedDocumentNewPath = DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss-fff", CultureInfo.InvariantCulture);
+                linkedDocumentNewPath = System.IO.Path.ChangeExtension(linkedDocumentNewPath, linkedDocumentExtension);
+                linkedDocumentNewPath = System.IO.Path.Combine(linkedDocumentDirectory, linkedDocumentNewPath);
+
+                // Sleep for 1 millisecond to avoid filename collision. Only relevant for this example.
+                System.Threading.Thread.Sleep(1);
+
+                // Populate the arrays.
+                ListOfInputFileNames.Add(linkedDocumentPath);
+                ListOfNewFileNames.Add(linkedDocumentNewPath);
+                ListOfInputActions.Add(RevisionManager.RevisionManagerAction.RenameAction);
+            }
+
+            // Set the action.
+            application.SetActionInRevisionManager(ListOfInputFileNames.Count, ListOfInputFileNames.ToArray(), ListOfInputActions.ToArray(), ListOfNewFileNames.ToArray());
+
+            // Perform the action.
+            application.PerformActionInRevisionManager();
+
+            // Close the assembly.
+            assemblyDocument.Close();
+
+            // Close Revision Manager.
+            application.Quit();
+        }
+    }
+}
---
+++ RevisionManager/PerformRenameAction/vb/PerformRenameAction/Program.vb
@@ -0,0 +1,65 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Globalization
+Imports System.Linq
+Imports System.Text
+
+Namespace PerformRenameAction
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+			' For testing purposes, change the path to the .asm.
+			Dim assemblyPath = "C:\Users\jason\Desktop\Asm3.asm"
+
+			' Start Revision Manager.
+			Dim application = New RevisionManager.Application()
+
+			' Open the assembly.
+			Dim assemblyDocument = DirectCast(application.OpenFileInRevisionManager(assemblyPath), RevisionManager.Document)
+
+			' Get the linked documents.
+			Dim linkedDocuments = DirectCast(assemblyDocument.LinkedDocuments, RevisionManager.LinkedDocuments)
+
+			' Allocate input arrays.
+			Dim ListOfInputFileNames = New List(Of String)()
+			Dim ListOfNewFileNames = New List(Of String)()
+			Dim ListOfInputActions = New List(Of RevisionManager.RevisionManagerAction)()
+
+			' Process each linked document.
+			For i As Integer = 1 To linkedDocuments.Count
+				' Get the specified linked document by index.
+				Dim linkedDocument = DirectCast(linkedDocuments.Item(i), RevisionManager.Document)
+
+				' Get the current path, folder path, filename and extension to the linked document.
+				Dim linkedDocumentPath = linkedDocument.AbsolutePath
+				Dim linkedDocumentDirectory = System.IO.Path.GetDirectoryName(linkedDocumentPath)
+				Dim linkedDocumentFilename = System.IO.Path.GetFileName(linkedDocumentPath)
+				Dim linkedDocumentExtension = System.IO.Path.GetExtension(linkedDocumentPath)
+
+				' Generate a new random filename for the linked document.
+				Dim linkedDocumentNewPath = Date.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss-fff", CultureInfo.InvariantCulture)
+				linkedDocumentNewPath = System.IO.Path.ChangeExtension(linkedDocumentNewPath, linkedDocumentExtension)
+				linkedDocumentNewPath = System.IO.Path.Combine(linkedDocumentDirectory, linkedDocumentNewPath)
+
+				' Sleep for 1 millisecond to avoid filename collision. Only relevant for this example.
+				System.Threading.Thread.Sleep(1)
+
+				' Populate the arrays.
+				ListOfInputFileNames.Add(linkedDocumentPath)
+				ListOfNewFileNames.Add(linkedDocumentNewPath)
+				ListOfInputActions.Add(RevisionManager.RevisionManagerAction.RenameAction)
+			Next i
+
+			' Set the action.
+			application.SetActionInRevisionManager(ListOfInputFileNames.Count, ListOfInputFileNames.ToArray(), ListOfInputActions.ToArray(), ListOfNewFileNames.ToArray())
+
+			' Perform the action.
+			application.PerformActionInRevisionManager()
+
+			' Close the assembly.
+			assemblyDocument.Close()
+
+			' Close Revision Manager.
+			application.Quit()
+		End Sub
+	End Class
+End Namespace
