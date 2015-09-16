83e1b269398fb292fdb6124f62375d9cd021e4f1
Jason Newell <jason.newell@outlook.com>
2015-09-15 23:19:05 -0500

    Added StressTest sample.

---
+++ General/StressTest/vb/StressTest/OpenCloseTask.vb
@@ -0,0 +1,59 @@
+﻿Imports SolidEdgeCommunity
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace StressTest
+    Public Class OpenCloseTask
+        Inherits IsolatedTaskProxy
+
+        Public Sub DoOpenClose(ByVal fileName As String)
+            InvokeSTAThread(Of String)(AddressOf DoOpenCloseInternal, fileName)
+        End Sub
+
+        Private Sub DoOpenCloseInternal(ByVal fileName As String)
+            'SolidEdgeFramework.Application application = null;
+            Dim documents As SolidEdgeFramework.Documents = Nothing
+'INSTANT VB NOTE: The variable document was renamed since Visual Basic does not handle local variables named the same as class members well:
+            Dim document_Renamed As SolidEdgeFramework.SolidEdgeDocument = Nothing
+
+            ' Register with OLE to handle concurrency issues on the current thread.
+            SolidEdgeCommunity.OleMessageFilter.Register()
+
+            Try
+                ' Get reference to application object.
+'INSTANT VB NOTE: The variable application was renamed since Visual Basic does not handle local variables named the same as class members well:
+                Dim application_Renamed = Me.Application
+
+                ' Get reference to documents collection.
+                documents = application_Renamed.Documents
+
+                ' Open the document.
+                document_Renamed = DirectCast(documents.Open(fileName), SolidEdgeFramework.SolidEdgeDocument)
+
+                ' Not sure why but as of ST8, I had to add a Sleep() call to prevent crashing...
+                System.Threading.Thread.Sleep(500)
+
+                ' Do idle processing.
+                application_Renamed.DoIdle()
+
+                ' Close the document.
+                document_Renamed.Close(False)
+
+                'System.Threading.Thread.Sleep(500);
+
+                ' Do idle processing.
+                application_Renamed.DoIdle()
+
+                documents.Close()
+            Catch ex As System.Exception
+                Console.WriteLine(ex.Message)
+            Finally
+                SolidEdgeCommunity.OleMessageFilter.Unregister()
+            End Try
+        End Sub
+    End Class
+End Namespace
---
+++ General/StressTest/vb/StressTest/Program.vb
@@ -0,0 +1,70 @@
+﻿Imports SolidEdgeCommunity
+Imports System
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace StressTest
+    Friend Class Program
+        Shared Sub Main(ByVal args() As String)
+            ' Start Solid Edge.
+            Dim application = SolidEdgeUtils.Connect(True, True)
+
+            ' Disable (most) prompts.
+            application.DisplayAlerts = False
+
+            Dim process = System.Diagnostics.Process.GetProcessById(application.ProcessID)
+            Dim initialHandleCount = process.HandleCount
+            Dim initialWorkingSet = process.WorkingSet64
+            Dim trainingFolderPath = SolidEdgeCommunity.SolidEdgeUtils.GetTrainingFolderPath()
+
+            For Each fileName In Directory.EnumerateFiles(trainingFolderPath, "*.*", SearchOption.AllDirectories)
+                Dim extension = Path.GetExtension(fileName)
+
+                If (extension.Equals(".asm", StringComparison.OrdinalIgnoreCase)) OrElse (extension.Equals(".dft", StringComparison.OrdinalIgnoreCase)) OrElse (extension.Equals(".par", StringComparison.OrdinalIgnoreCase)) OrElse (extension.Equals(".psm", StringComparison.OrdinalIgnoreCase)) OrElse (extension.Equals(".pwd", StringComparison.OrdinalIgnoreCase)) Then
+                    Console.WriteLine("Opening & closing '{0}'", fileName)
+
+                    Dim startHandleCount = process.HandleCount
+                    Dim startWorkingSet = process.WorkingSet64
+
+                    Using task = New IsolatedTask(Of OpenCloseTask)()
+                        task.Proxy.Application = application
+                        task.Proxy.DoOpenClose(fileName)
+                    End Using
+
+                    process.Refresh()
+
+                    Dim endHandleCount = process.HandleCount
+                    Dim endWorkingSet = process.WorkingSet64
+
+                    Console.WriteLine("Handle count start" & ControlChars.Tab & ControlChars.Tab & "'{0}'", startHandleCount)
+                    Console.WriteLine("Handle count end" & ControlChars.Tab & ControlChars.Tab & "'{0}'", endHandleCount)
+                    Console.WriteLine("Handle count change" & ControlChars.Tab & ControlChars.Tab & "'{0}'", endHandleCount - startHandleCount)
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("Handle count total change" & ControlChars.Tab & "'{0}'", endHandleCount - initialHandleCount)
+                    Console.ForegroundColor = ConsoleColor.Gray
+                    Console.WriteLine("Working set start" & ControlChars.Tab & ControlChars.Tab & "'{0}'", startWorkingSet)
+                    Console.WriteLine("Working set end" & ControlChars.Tab & ControlChars.Tab & ControlChars.Tab & "'{0}'", endWorkingSet)
+                    Console.WriteLine("Working set change" & ControlChars.Tab & ControlChars.Tab & "'{0}'", endWorkingSet - startWorkingSet)
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine("Working set total change" & ControlChars.Tab & "'{0}'", endWorkingSet - initialWorkingSet)
+                    Console.ForegroundColor = ConsoleColor.Gray
+
+
+                    Console.WriteLine()
+                End If
+            Next fileName
+
+            If application IsNot Nothing Then
+                application.DisplayAlerts = True
+            End If
+
+#If DEBUG Then
+            System.Diagnostics.Debugger.Break()
+#End If
+        End Sub
+    End Class
+End Namespace
