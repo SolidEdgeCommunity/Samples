4422a101910de84bda36bf19916d324575c8b5eb
Jason Newell <jason.newell@outlook.com>
2016-10-23 15:26:11 -0500

    Added powershell utility scripts

---
+++ BuildSolutions.ps1
@@ -0,0 +1,31 @@
+﻿function DoBuild
+{
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [String] $path
+    )
+    process
+    {
+        $msBuildExe = 'C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe'
+
+        Write-Host "Cleaning $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Clean /m /consoleloggerparameters:ErrorsOnly
+
+        Write-Host "Building $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Build /m /consoleloggerparameters:ErrorsOnly
+    }
+}
+
+$samples_path = $PSScriptRoot
+
+cls
+
+Write-Host "Processing folder $samples_path"
+
+pwd
+
+foreach ($solution in Get-ChildItem $samples_path -Recurse -Include *.sln)
+{
+    DoBuild $solution.FullName
+}
---
+++ UpdatePackages.ps1
@@ -0,0 +1,29 @@
+﻿function DoUpdate
+{
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [String] $path
+    )
+    process
+    {
+        Write-Host "nuget restore $($path)" -foregroundcolor green
+        nuget restore "$($path)"
+
+        Write-Host "nuget update $($path)" -foregroundcolor green
+        nuget update "$($path)"
+    }
+}
+
+$samples_path = $PSScriptRoot
+
+cls
+Write-Host $PSCommandPath
+Write-Host "Processing folder $samples_path"
+
+pwd
+
+foreach ($solution in Get-ChildItem $samples_path -Recurse -Include *.sln)
+{
+    DoUpdate $solution.FullName
+}
